23507fa5f1905aa1b8d4d04ac5e40cd3ac5c3911
Zhouyang <zhouyang@surgsmart.com>
2026-04-26 18:56:47 +0800

    update

--- train_needle_gold_suture.sh
+++ train_needle_gold_suture.sh
@@ -1,4 +1,6 @@
-python main.py fit \
+PYTHON_BIN="${PYTHON_BIN:-/home/zhouyang/anaconda3/envs/eomt/bin/python}"
+
+"${PYTHON_BIN}" main.py fit \
     -c configs/dinov3/needle_gold_suture/instance/eomt_base_640_ft.yaml \
     --trainer.devices 4 \
     --data.batch_size 8 \
